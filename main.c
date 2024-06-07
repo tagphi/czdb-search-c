b03daf12ed7d147543b571aca8de751626af4b7d
刘聪 <cong.liu@rtbasia.com>
2024-06-08 00:38:16 +0800

    改成交互输入

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
 #include "db_searcher.h"
+#include <string.h>
+#include <unistd.h>
 
-int main(void) {
-    DBSearcher* dbSearcher = initDBSearcher("/Users/liucong/Downloads/offline_db.czdb", "3fEhuZUEvDzRjKv9qvAzTQ==", BTREE);
+int main(int argc, char *argv[]) {
+    char* dbPath = "/Users/liucong/Downloads/offline_db.czdb";
+    char* key = "3fEhuZUEvDzRjKv9qvAzTQ==";
+    int opt;
+
+    while ((opt = getopt(argc, argv, "p:k:")) != -1) {
+        switch (opt) {
+        case 'p':
+            dbPath = optarg;
+            break;
+        case 'k':
+            key = optarg;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-p dbPath] [-k key]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    DBSearcher* dbSearcher = initDBSearcher(dbPath, key, BTREE);
 
     info(dbSearcher);
 
     char region[100];
-    search("1.32.240.0", dbSearcher, region, 100);
+    char ip[50];
 
-    printf("Region: %s\n", region);
+    while (1) {
+        printf("Enter IP address (or 'q' to quit): ");
+        fgets(ip, 50, stdin);
+
+        // Remove trailing newline
+        ip[strcspn(ip, "\n")] = 0;
+
+        if (strcmp(ip, "q") == 0) {
+            break;
+        }
+
+        if (search(ip, dbSearcher, region, 100) == 0) {
+            printf("Region: %s\n", region);
+        } else {
+            printf("IP not found or an error occurred.\n");
+        }
+    }
 
     closeDBSearcher(dbSearcher);
 
